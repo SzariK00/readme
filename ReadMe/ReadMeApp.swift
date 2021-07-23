3bf59b83e6cb3f8d28820f96b56c07998df0fbd4
Adrian Jelonek <ajelonek@gmail.com>
2021-07-23 23:15:12 +0200

    First commit. App dev start.

---
+++ ReadMe/ReadMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  ReadMeApp.swift
+//  ReadMe
+//
+//  Created by Adrian Jelonek on 23/07/2021.
+//
+
+import SwiftUI
+
+@main
+struct ReadMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
